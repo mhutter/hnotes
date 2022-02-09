902d710f945ffede24ab6961e7102eaf91109e2e
Manuel Hutter <manuel@hutter.io>
2022-02-09 17:44:35 +0100

    Cleanup :broom:
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- src/bar.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-if($sprache == 'en') {
-	$c["bar"] = 'Logged in as '.$logged_as.' - <a href="/settings">Settings</a> - <a href="/logout">Logout</a>';
-} else {
-	$c["bar"] = 'Eingeloggt als '.$logged_as.' - <a href="/settings">Einstellungen</a> - <a href="/logout">Abmelden</a>';
-}
-
-?>
